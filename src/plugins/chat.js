e622b4814956f49699269fde13070644e9ef108b
s4kr4 <s4kr4m4@gmail.com>
2019-11-06 02:58:38 +0900

    Refactor chat plugin

--- src/plugins/chat.js
+++ src/plugins/chat.js
@@ -4,11 +4,27 @@ const replacers = (new Replacers)
   .add(/^<(.*?)>\s(.*)$/, (message, player, text) => `**${player}**: ${text}`)
   .add(/^\[(.*?)]\s(.*)$/, (message, player, text) => `**${player}**: ${text}`)
 
+const generateMessage = (message) => {
+
+  console.dir(message.attachments.size)
+  if (message.attachments.size > 0) {
+    return {
+      text: `[Discord] ${message.member && message.member.nickname || message.author.username}> が画像を送信しました`,
+      italic: true,
+      color: "aqua",
+    }
+  }
+
+  return {
+    text: `<${message.member && message.member.nickname || message.author.username}> ${message.cleanContent}`
+  }
+}
+
 export default Plugin => new Plugin({
   async discord ({message, sendToMinecraft}) {
-    await sendToMinecraft(`tellraw @a ${JSON.stringify({
-      text: `<${message.member && message.member.nickname || message.author.username}> ${message.cleanContent}`
-    })}`)
+    if (/^!sh/.test(message.cleanContent)) return
+
+    await sendToMinecraft(`tellraw @a ${JSON.stringify(generateMessage(message))}`)
   },
   async minecraft ({causedAt, level, message, sendToDiscord}) {
     if (!/^Async\sChat\sThread/.test(causedAt)) return
