778b8ebbd794dc6ddb880e91f9f4c4669e2edf81
s4kr4 <s4kr4m4@gmail.com>
2019-11-06 23:36:19 +0900

    Show death message

--- src/plugins/chat.js
+++ src/plugins/chat.js
@@ -1,4 +1,5 @@
 import Replacers from '../Replacers'
+import dictionary from './dictionary'
 
 const replacers = (new Replacers)
   .add(/^<(.*?)>\s(.*)$/, (message, player, text) => `**${player}**: ${text}`)
@@ -19,6 +20,24 @@ const generateMessage = (message) => {
   }
 }
 
+const translateLogs = (message) => {
+  let translated = ''
+  dictionary.map((sentense) => {
+    const matched = message.match(sentense.regex)
+    if (matched) {
+      if (matched.length === 2) {
+        translated = sentense.japanese.replace('player', matched[1])
+      } else if (matched.length === 3) {
+        translated = sentense.japanese
+          .replace('player', matched[1])
+          .replace('mob', matched[2])
+      }
+    }
+  })
+
+  return translated
+}
+
 export default Plugin => new Plugin({
   async discord ({message, sendToMinecraft}) {
     if (/^!sh/.test(message.cleanContent)) return
@@ -26,9 +45,12 @@ export default Plugin => new Plugin({
     await sendToMinecraft(`tellraw @a ${JSON.stringify(generateMessage(message))}`)
   },
   async minecraft ({causedAt, level, message, sendToDiscord}) {
-    if (!/^Async\sChat\sThread/.test(causedAt)) return
-
-    const newMessage = replacers.replace(message)
-    if (newMessage !== false) await sendToDiscord(newMessage.replace('\u001b[m', ''))
+    if (/^Async\sChat\sThread/.test(causedAt)) {
+      const newMessage = replacers.replace(message)
+      if (newMessage !== false) await sendToDiscord(newMessage.replace('\u001b[m', ''))
+    } else if (/^Server\sthread/.test(causedAt)) {
+      const translated = translateLogs(message)
+      if (translated) await sendToDiscord(`**${translated}**`)
+    }
   }
 })
---
+++ src/plugins/dictionary.js
@@ -0,0 +1,40 @@
+const dictionary = [
+  {
+    regex: /^(.*) drowned/,
+    japanese: 'playerは溺れ死んだ',
+  },
+  {
+    regex: /^(.*) tried to swim in lava$/,
+    japanese: 'playerは溶岩遊泳を試みた',
+  },
+  {
+    regex: /^(.*) burned to death$/,
+    japanese: 'playerはこんがりと焼けてしまった',
+  },
+  {
+    regex: /^(.*) fell from a high place$/,
+    japanese: 'playerは高い所から落ちた',
+  },
+  {
+    regex: /^(.*) was shot by (.*)$/,
+    japanese: 'playerはmobに射抜かれた',
+  },
+  {
+    regex: /^(.*) was impaled by (.*)$/,
+    japanese: 'playerはmobによって突き抜かれた',
+  },
+  {
+    regex: /^(.*) was slain by (.*)$/,
+    japanese: 'playerはmobに殺害された',
+  },
+  {
+    regex: /^(.*) was blown up by (.*)$/,
+    japanese: 'playerはmobに爆破されてしまった',
+  },
+  {
+    regex: /^(.*) was poked to death by a sweet berry bush$/,
+    japanese: 'playerはスイートベリーの棘が刺さって死んだ',
+  },
+]
+
+export default dictionary
