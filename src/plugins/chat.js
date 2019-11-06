45d7b3d892224cdbf0447ddc2d0e99df5e3cae16
s4kr4 <s4kr4m4@gmail.com>
2019-11-06 23:35:52 +0900

    Update message

--- src/plugins/chat.js
+++ src/plugins/chat.js
@@ -8,7 +8,7 @@ const generateMessage = (message) => {
 
   if (message.attachments.size > 0) {
     return {
-      text: `[Discord] ${message.member && message.member.nickname || message.author.username}> が画像を送信しました`,
+      text: `[Discord] ${message.member && message.member.nickname || message.author.username} が画像を送信しました`,
       italic: true,
       color: "aqua",
     }
